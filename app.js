0c7c4a807e362c2f5a915703f99c697dc7678cd2
nikhil <nikhilcares22@gmail.com>
2021-05-23 12:47:58 +0530

    completed priority queues

---
+++ app.js
@@ -0,0 +1,73 @@
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(val,priority) {
+        let newNode = new Node(val,priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if (element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    dequeue() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown()
+        }
+        return max;
+    }
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority < element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap && rightChild.priority < leftChild.priority) || (!swap && rightChild.priority < element.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (!swap) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+
+}
+class Node {
+    constructor(val,priority){
+        this.val = val;
+        this.priority = priority;
+    }
+}
+let heap = new PriorityQueue();
+heap.enqueue("common cold",5)
+heap.enqueue("gunshot wound",1)
+heap.enqueue("corona",2)
+console.log(heap.dequeue())
+console.log(heap)
